4645336057a693932d072c786f1a51b0420deac4
olivar16 <olivar16@up.edu>
2015-04-13 21:26:29 -0700

    Made changes

--- tracks.js
+++ tracks.js
@@ -16,9 +16,6 @@ $('#genres').on('change', function() {
 
 function overallTracks(){
     
-    alert("Not implemented yet");
-    
-    
     
 }
 
@@ -34,6 +31,20 @@ var requestUrl = 'https://api.spotify.com/v1/browse/categories/'+genre+'/playlis
        'Authorization': 'Bearer ' + accessToken
    },
    success: function(response) {
+    $("#playlistSelection").html("");
+$("#playlistSelection").append("<select id ='playlists'>");
+
+        playlistIds ={};
+
+        for(i=0;i<response.playlists.items[i].length;i++){
+        playlistIDs[response.playlists.items[i].name]=response.playlists.items[i].id;
+        $("#playlistSelection").append("<option value='"+response.playlists.items[i].id+"'>"+response.playlists.items[i].name+"</option>");
+
+
+                                                        }
+
+$("#playlistSelection").append("</select>");
+        //iterate through every playlist and create a list 
        console.log(response);
        console.log("The ID is "+response.playlists.items[0].id);;
        getPlaylistTracks(response.playlists.items[0].id);
@@ -42,5 +53,19 @@ var requestUrl = 'https://api.spotify.com/v1/browse/categories/'+genre+'/playlis
     
     
     
+}
+
+function selectPlaylist(){
+
+$("#playlistSelection").append("");
+
+
+}
+
+function appendPlaylists(name, id){
+
+
+
+
 }
 
