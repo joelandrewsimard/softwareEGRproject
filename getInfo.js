f7d904ad2d3cf47c11f240a20516684a27a95e35
olivar16 <olivar16@up.edu>
2015-04-21 15:22:50 -0700

    Made changes

--- getInfo.js
+++ getInfo.js
@@ -418,6 +418,7 @@ $.ajax({
        'Authorization': 'Bearer ' + accessToken
    },
    success: function(response) {
+    $("#search_results").html("");
     $("#search_results").append("<h3>Barfing user info</h3><br>");
     $("#search_results").append('<iframe src="//giphy.com/embed/moZM0cd3y7x2U" width="200" height="200" frameBorder="0" style="max-width: 100%" class="giphy-embed" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe><br>');
         
